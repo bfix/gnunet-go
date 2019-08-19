2abad1baf8e62dd2baef7f302592aed89a8abcf4
Bernd Fix <brf@hoi-polloi.org>
2019-08-19 19:54:14 +0200

    Simplified build script.

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-function compile() {
-	GOPATH=$(pwd) go build -o bin/$1 src/cmd/$1/main.go
-}
-
-compile vanityid
-compile gns_service
-
+GOPATH=$(pwd) go install -v -gcflags "-N -l" ./...
