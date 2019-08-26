a112e400188520107067d2edae1b4cf3a68ae2c1
Bernd Fix <brf@hoi-polloi.org>
2019-08-26 23:09:47 +0200

    Added method comments.

--- src/gnunet/crypto/key_derivation.go
+++ src/gnunet/crypto/key_derivation.go
@@ -8,9 +8,9 @@ import (
 	"gnunet/crypto/hkdf"
 )
 
-////////////////////////////////////////////////////////////////////////
-
-// DeriveH
+// DeriveH derives an integer 'h' in the range [0,n[ with 'n' being the
+// order of the underlying Ed25519 curve. The value of 'h' is derived
+// from the arguments.
 func (pub *PublicKey) DeriveH(label, context string) *big.Int {
 	prk := hkdf.Extract(sha512.New, pub.Bytes(), []byte("key-derivation"))
 	data := append([]byte(label), []byte(context)...)
@@ -21,7 +21,8 @@ func (pub *PublicKey) DeriveH(label, context string) *big.Int {
 	return new(big.Int).Mod(h, n)
 }
 
-// PublicKeyDerive
+// PublicKeyDerive "shifts" a public key 'Q' to a new point 'P' where
+// P = h*Q with 'h' being a factor derived from the arguments.
 func (pub *PublicKey) DeriveKey(label string, context string) *PublicKey {
 	h := pub.DeriveH(label, context)
 	return pub.Mult(h)
