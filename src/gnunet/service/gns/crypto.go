1de6b903a8957e3daf256452537fa09693f46acf
Bernd Fix <brf@hoi-polloi.org>
2019-09-02 00:39:30 +0200

    Create DHT query from (PKEY,label) values.

--- src/gnunet/service/gns/crypto.go
+++ src/gnunet/service/gns/crypto.go
@@ -1,36 +1,12 @@
 package gns
 
 import (
-//	"gnunet/crypto"
+	"gnunet/crypto"
 )
 
-/**
- * Calculate the DHT query for a given @a label in a given @a zone.
- *
- * @param pub public key of the zone
- * @param label label of the record
- * @param query hash to use for the query
- *
-void
-GNUNET_GNSRECORD_query_from_public_key (const struct GNUNET_CRYPTO_EcdsaPublicKey *pub,
-					const char *label,
-					struct GNUNET_HashCode *query)
-{
-  struct GNUNET_CRYPTO_EcdsaPublicKey pd;
-
-  GNUNET_CRYPTO_ecdsa_public_key_derive (pub,
-                                         label,
-                                         "gns",
-                                         &pd);
-  GNUNET_CRYPTO_hash (&pd,
-                      sizeof (pd),
-                      query);
-}
-
-
-// QueryFromPublickeyDerive
+// QueryFromPublickeyDerive calculates the DHT query for a given label in a
+// given zone (identified by PKEY).
 func QueryFromPublickeyDerive(pkey *crypto.PublicKey, label string) *crypto.HashCode {
-	pd := crypto.PublicKeyDerive(pkey, label, "gns")
+	pd := pkey.DeriveKey(label, "gns")
 	return crypto.Hash(pd.Bytes())
 }
-*/
