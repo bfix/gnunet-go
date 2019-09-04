83f5b390c16fb6cbc9332b368cc5e7f54f1b7160
Bernd Fix <brf@hoi-polloi.org>
2019-09-05 01:39:26 +0200

    Added helper for simple client request with single response.

--- src/gnunet/service/client.go
+++ src/gnunet/service/client.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/bfix/gospel/logger"
 	"gnunet/message"
 	"gnunet/transport"
 )
@@ -32,4 +33,27 @@ func (c *Client) ReceiveResponse() (message.Message, error) {
 
 func (c *Client) Close() error {
 	return c.ch.Close()
+}
+
+func ServiceRequestResponse(caller, callee, endp string, req message.Message) (message.Message, error) {
+	// client-connect to the service
+	logger.Printf(logger.DBG, "[%s] Connect to %s service\n", caller, callee)
+	cl, err := NewClient(endp)
+	if err != nil {
+		return nil, err
+	}
+	// send request
+	logger.Printf(logger.DBG, "[%s] Sending request to %s service\n", caller, callee)
+	if err = cl.SendRequest(req); err != nil {
+		return nil, err
+	}
+	// wait for a single response, then close the connection
+	logger.Printf(logger.DBG, "[%s] Waiting for response from %s service\n", caller, callee)
+	var resp message.Message
+	if resp, err = cl.ReceiveResponse(); err != nil {
+		return nil, err
+	}
+	logger.Printf(logger.DBG, "[%s] Closing connection to %s service\n", caller, callee)
+	cl.Close()
+	return resp, nil
 }
