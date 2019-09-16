7e355854b5e7f046348e8be396af4ca6a9f2a103
Bernd Fix <brf@hoi-polloi.org>
2019-09-16 20:47:45 +0200

    Implemented block decryption.

--- src/gnunet/service/gns/crypto.go
+++ src/gnunet/service/gns/crypto.go
@@ -1,8 +1,12 @@
 package gns
 
 import (
+	"crypto/sha256"
+	"crypto/sha512"
+
 	"github.com/bfix/gospel/crypto/ed25519"
 	"gnunet/crypto"
+	"golang.org/x/crypto/hkdf"
 )
 
 // QueryFromPublickeyDerive calculates the DHT query for a given label in a
@@ -13,6 +17,25 @@ func QueryFromPublickeyDerive(pkey *ed25519.PublicKey, label string) *crypto.Has
 }
 
 // DecryptBlock
-func (s *GNSService) DecryptBlock(pkey *ed25519.PublicKey, sig *ed25519.EcSignature, data []byte) ([]byte, error) {
-	return data, nil
+func DecryptBlock(data []byte, zoneKey *ed25519.PublicKey, label string) (out []byte, err error) {
+	// derive key material for decryption
+	iv, skey := deriveBlockKey(label, zoneKey)
+	// perform decryption
+	return crypto.SymmetricDecrypt(data, skey, iv)
+}
+
+// Derive a symmetric key to decipher a GNS block
+func deriveBlockKey(label string, pub *ed25519.PublicKey) (iv *crypto.SymmetricIV, skey *crypto.SymmetricKey) {
+	// generate symmetric key
+	prk := hkdf.Extract(sha512.New, []byte(label), pub.Bytes())
+	rdr := hkdf.Expand(sha256.New, prk, []byte("gns-aes-ctx-key"))
+	skey = new(crypto.SymmetricKey)
+	rdr.Read(skey.AESKey)
+	rdr.Read(skey.TwofishKey)
+	// generate initialization vector
+	rdr = hkdf.Expand(sha256.New, prk, []byte("gns-aes-ctx-iv"))
+	iv = new(crypto.SymmetricIV)
+	rdr.Read(iv.AESIv)
+	rdr.Read(iv.TwofishIv)
+	return
 }
