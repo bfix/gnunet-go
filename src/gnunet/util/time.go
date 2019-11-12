9cbf4e0cd9a88281e0cf16cd08de506b39ebb142
Bernd Fix <brf@hoi-polloi.org>
2019-11-12 10:08:39 +0100

    Add helper for absolute time "Never".

--- src/gnunet/util/time.go
+++ src/gnunet/util/time.go
@@ -9,10 +9,14 @@ import (
 // Absolute time
 //----------------------------------------------------------------------
 
+// AbsoluteTime refers to a unique point in time.
+// The value is the elapsed time in milliseconds (Unix epoch), so no timestamp
+// before January 1st, 1970 is possible (not a restriction for GNUnet).
 type AbsoluteTime struct {
 	Val uint64 `order:"big"`
 }
 
+// NewAbsoluteTime set the point in time to the given time value
 func NewAbsoluteTime(t time.Time) AbsoluteTime {
 	secs := t.Unix()
 	usecs := t.Nanosecond() / 1000
@@ -21,10 +25,17 @@ func NewAbsoluteTime(t time.Time) AbsoluteTime {
 	}
 }
 
+// AbsoluteTimeNow returns the current point in time.
 func AbsoluteTimeNow() AbsoluteTime {
 	return NewAbsoluteTime(time.Now())
 }
 
+// AbsoluteTimeNever returns the time defined as "never"
+func AbsoluteTimeNever() AbsoluteTime {
+	return AbsoluteTime{math.MaxUint64}
+}
+
+// String returns a human-readable notation of an absolute time.
 func (t AbsoluteTime) String() string {
 	if t.Val == math.MaxUint64 {
 		return "Never"
@@ -33,12 +44,14 @@ func (t AbsoluteTime) String() string {
 	return ts.Format(time.RFC3339Nano)
 }
 
+// Add a duration to an absolute time yielding a new absolute time.
 func (t AbsoluteTime) Add(d time.Duration) AbsoluteTime {
 	return AbsoluteTime{
 		Val: t.Val + uint64(d.Milliseconds()),
 	}
 }
 
+// Expired returns true if the timestamp is in the past.
 func (t AbsoluteTime) Expired() bool {
 	// check for "never"
 	if t.Val == math.MaxUint64 {
@@ -51,16 +64,20 @@ func (t AbsoluteTime) Expired() bool {
 // Relative time
 //----------------------------------------------------------------------
 
+// Relative time is a timestamp defined relative to the current time.
+// It actually is more like a duration than a time...
 type RelativeTime struct {
 	Val uint64 `order:"big"`
 }
 
+// NewRelativeTime is initialized with a given duration.
 func NewRelativeTime(d time.Duration) RelativeTime {
 	return RelativeTime{
 		Val: uint64(d.Milliseconds()),
 	}
 }
 
+// String returns a human-readble representation of a relative time (duration).
 func (t RelativeTime) String() string {
 	if t.Val == math.MaxUint64 {
 		return "Forever"
