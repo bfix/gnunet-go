f038179c3dd4812c54e2ce87133666afc396b27f
Bernd Fix <brf@hoi-polloi.org>
2019-11-12 10:17:58 +0100

    Added string list helpers.

--- src/gnunet/util/array.go
+++ src/gnunet/util/array.go
@@ -4,30 +4,28 @@ import (
 	"fmt"
 )
 
+// Error variables
 var (
 	ErrUtilArrayTooSmall = fmt.Errorf("Array to small")
 )
 
+//----------------------------------------------------------------------
+// Byte array helpers
+//----------------------------------------------------------------------
+
+// Clone creates a new array of same content as the argument.
 func Clone(d []byte) []byte {
 	r := make([]byte, len(d))
 	copy(r, d)
 	return r
 }
 
+// Reverse the content of a byte array
 func Reverse(b []byte) []byte {
 	bl := len(b)
 	r := make([]byte, bl)
 	for i := 0; i < bl; i++ {
 		r[bl-i-1] = b[i]
-	}
-	return r
-}
-
-func ReverseStrings(s []string) []string {
-	sl := len(s)
-	r := make([]string, sl)
-	for i := 0; i < sl; i++ {
-		r[sl-i-1] = s[i]
 	}
 	return r
 }
@@ -50,8 +48,40 @@ func CopyBlock(out, in []byte) {
 	copy(out[to:], in[from:])
 }
 
+// Fill an array with a value
 func Fill(b []byte, val byte) {
 	for i := 0; i < len(b); i++ {
 		b[i] = val
 	}
+}
+
+//----------------------------------------------------------------------
+// String list helpers
+//----------------------------------------------------------------------
+
+// Reverse StringList reverse an array of strings
+func ReverseStringList(s []string) []string {
+	sl := len(s)
+	r := make([]string, sl)
+	for i := 0; i < sl; i++ {
+		r[sl-i-1] = s[i]
+	}
+	return r
+}
+
+// Convert a binary representation of a string list. Each string is '\0'-
+// terminated. The whole byte array is parsed; if the final string is not
+// terminated, it is skipped.
+func StringList(b []byte) []string {
+	res := make([]string, 0)
+	str := ""
+	for _, ch := range b {
+		if ch == 0 {
+			res = append(res, str)
+			str = ""
+			continue
+		}
+		str += string(ch)
+	}
+	return res
 }
