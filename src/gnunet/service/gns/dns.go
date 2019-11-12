5177d73e3300ecdd8a85fbd8bbcdb0329d39d23b
Bernd Fix <brf@hoi-polloi.org>
2019-11-12 12:25:47 +0100

    Helper to parse RFC1034 names added.

--- src/gnunet/service/gns/dns.go
+++ src/gnunet/service/gns/dns.go
@@ -22,10 +22,33 @@ var (
 	ErrNoDNSResults = fmt.Errorf("No valid DNS results")
 )
 
+// Convert DNS name from its binary representation [RFC1034]:
+// A string is a sequence of a (len,chars...) tupels terminated by a (len=0,).
+// The name parts are concatenated with "." as separator.
+// The parsing starts at offset in the byte array; the function returns the
+// offset after the parsed name as well as the name itself.
+func DNSNameFromBytes(b []byte, offset int) (int, string) {
+	if offset >= len(b) {
+		return offset, ""
+	}
+	str := ""
+	pos := offset
+	for b[pos] != 0 {
+		if len(str) > 0 {
+			str += "."
+		}
+		count := int(b[pos])
+		pos++
+		str += string(b[pos : pos+count])
+		pos += count
+	}
+	return pos + 1, str
+}
+
 // queryDNS resolves a name on a given nameserver and delivers all matching
 // resource record (of type 'kind') to the result channel.
 func queryDNS(id int, name string, server net.IP, kind int, res chan *GNSRecordSet) {
-	logger.Printf(logger.DBG, "[dns][%d] Starting query for '%s' on '%s'...", id, name, server.String())
+	logger.Printf(logger.DBG, "[dns][%d] Starting query for '%s' on '%s'...\n", id, name, server.String())
 
 	// assemble query
 	m := &dns.Msg{
@@ -53,15 +76,16 @@ func queryDNS(id int, name string, server net.IP, kind int, res chan *GNSRecordS
 		if err != nil {
 			errMsg := err.Error()
 			if strings.HasSuffix(errMsg, "i/o timeout") {
-				logger.Printf(logger.WARN, "[dns][%d] Query timed-out -- retrying (%d/5)", id, retry+1)
+				logger.Printf(logger.WARN, "[dns][%d] Query timed-out -- retrying (%d/5)\n", id, retry+1)
 				continue
 			}
-			logger.Printf(logger.ERROR, "[dns][%d] Error: %s", id, errMsg)
+			logger.Printf(logger.ERROR, "[dns][%d] Error: %s\n", id, errMsg)
 			res <- nil
 		}
 		// process results
+		logger.Printf(logger.WARN, "[dns][%d] Response from DNS server received (%d/5).\n", id, retry+1)
 		if in == nil {
-			logger.Printf(logger.ERROR, "[dns][%d] No results", id)
+			logger.Printf(logger.ERROR, "[dns][%d] No results\n", id)
 			res <- nil
 			return
 		}
@@ -79,20 +103,21 @@ func queryDNS(id int, name string, server net.IP, kind int, res chan *GNSRecordS
 			buf := make([]byte, 2048)
 			n, err := dns.PackRR(record, buf, 0, nil, false)
 			if err != nil {
-				logger.Printf(logger.WARN, "[dns][%d] Failed to get RR data for %s", id, err.Error())
+				logger.Printf(logger.WARN, "[dns][%d] Failed to get RR data for %s\n", id, err.Error())
 				continue
 			}
 			if n < int(rr.Size) {
-				logger.Printf(logger.WARN, "[dns][%d] Nit enough data in RR (%d != %d)", id, n, rr.Size)
+				logger.Printf(logger.WARN, "[dns][%d] Nit enough data in RR (%d != %d)\n", id, n, rr.Size)
 				continue
 			}
 			copy(rr.Data, buf[n-int(rr.Size):])
 			set.AddRecord(rr)
 		}
+		logger.Printf(logger.WARN, "[dns][%d] %d resource records extracted from response (%d/5).\n", id, set.Count, retry+1)
 		res <- set
 		return
 	}
-	logger.Printf(logger.WARN, "[dns][%d] Resolution failed -- giving up", id)
+	logger.Printf(logger.WARN, "[dns][%d] Resolution failed -- giving up...\n", id)
 	res <- nil
 }
 
@@ -100,7 +125,7 @@ func queryDNS(id int, name string, server net.IP, kind int, res chan *GNSRecordS
 // parallel; the first result delivered by any of the servers is returned
 // as the result list of matching resource records.
 func (gns *GNSModule) ResolveDNS(name string, servers []string, kind int, pkey *ed25519.PublicKey) (set *GNSRecordSet, err error) {
-	logger.Printf(logger.DBG, "[dns] Resolution of '%s' starting...", name)
+	logger.Printf(logger.DBG, "[dns] Resolution of '%s' starting...\n", name)
 
 	// start DNS queries concurrently
 	res := make(chan *GNSRecordSet)
@@ -116,14 +141,14 @@ func (gns *GNSModule) ResolveDNS(name string, servers []string, kind int, pkey *
 				srv = strings.TrimSuffix(srv, ".+")
 				set, err = gns.Resolve(srv, pkey, enums.GNS_TYPE_ANY, enums.GNS_LO_DEFAULT)
 				if err != nil {
-					logger.Printf(logger.ERROR, "[dns] Can't resolve NS server '%s' in '%s'", srv, zone)
+					logger.Printf(logger.ERROR, "[dns] Can't resolve NS server '%s' in '%s'\n", srv, zone)
 					continue
 				}
 			} else {
 				// resolve absolute GNS name (MUST end in a PKEY)
 				set, err = gns.Resolve(srv, nil, enums.GNS_TYPE_ANY, enums.GNS_LO_DEFAULT)
 				if err != nil {
-					logger.Printf(logger.ERROR, "[dns] Can't resolve NS server '%s'", srv)
+					logger.Printf(logger.ERROR, "[dns] Can't resolve NS server '%s'\n", srv)
 					continue
 				}
 			}
@@ -155,15 +180,18 @@ func (gns *GNSModule) ResolveDNS(name string, servers []string, kind int, pkey *
 			running--
 			if set != nil {
 				// we have a result.
+				logger.Println(logger.DBG, "[dns] Query result available.")
 				return
 			}
 			if running == 0 {
 				// no results
+				logger.Println(logger.WARN, "[dns] No results received from queries.")
 				return nil, ErrNoDNSResults
 			}
 
 		case <-timeout:
 			// no results
+			logger.Println(logger.WARN, "[dns] Queries timed out.")
 			return nil, ErrNoDNSResults
 		}
 	}
