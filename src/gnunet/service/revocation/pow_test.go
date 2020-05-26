02274607d6703a657f6258a2df7dfdf583673ec8
Bernd Fix <brf@hoi-polloi.org>
2020-05-26 13:59:10 +0200

    New test case for changed argon2 salt.

--- src/gnunet/service/revocation/pow_test.go
+++ src/gnunet/service/revocation/pow_test.go
@@ -23,46 +23,46 @@ var (
 	test_data = []testData{
 		{
 
-			"e01d304d45676849edcb36c843ad31837c9de8c7e58028a2e7c2a9894f130b6f", // private scalar D
-			"d2c825295cfd3073b6149c4393aa9483c51cfaf62731d2bf1127856913233b78", // public key
+			"90ea2a95cb9ef482b45817dc45b805cae00f387022a065a3674f41ad15173c63", // private scalar D
+			"4ac1e51d9a585a9ad9fb0dfac2be100aee83f0cc79c4c5ea8f3eb8afd9092fa5", // public key
 			"" +
-				"0005a5fc192e1d2c" + // timestamp
-				"0000395d1827c000" + // TTL
-				"f74d39f9ee9a7344" + // PoW_0
-				"f74d39f9ee9a7610" +
-				"f74d39f9ee9a7677" +
-				"f74d39f9ee9a7774" +
-				"f74d39f9ee9a777d" +
-				"f74d39f9ee9a77a3" +
-				"f74d39f9ee9a77ad" +
-				"f74d39f9ee9a77b9" +
-				"f74d39f9ee9a77de" +
-				"f74d39f9ee9a7851" +
-				"f74d39f9ee9a786f" +
-				"f74d39f9ee9a78a3" +
-				"f74d39f9ee9a78ba" +
-				"f74d39f9ee9a78ca" +
-				"f74d39f9ee9a7916" +
-				"f74d39f9ee9a79a9" +
-				"f74d39f9ee9a7a37" +
-				"f74d39f9ee9a7a57" +
-				"f74d39f9ee9a7a5c" +
-				"f74d39f9ee9a7a9e" +
-				"f74d39f9ee9a7ad3" +
-				"f74d39f9ee9a7b1b" +
-				"f74d39f9ee9a7b7b" +
-				"f74d39f9ee9a7b83" +
-				"f74d39f9ee9a7b8b" +
-				"f74d39f9ee9a7bbe" +
-				"f74d39f9ee9a7bcc" +
-				"f74d39f9ee9a7be6" +
-				"f74d39f9ee9a7c2b" +
-				"f74d39f9ee9a7c5b" +
-				"f74d39f9ee9a7c5f" +
-				"f74d39f9ee9a7c83" + // PoW_31
-				"05b94e2ad6496a8938aaf122f91edbacf2401cce8ec02e551e2a4433e0a76256" + // Sig.R
-				"09195bbe7636e9fd9076f8f20bc62467cc8371c487e7809efeaeb6ef7178b623" + // Sig.S
-				"d2c825295cfd3073b6149c4393aa9483c51cfaf62731d2bf1127856913233b78", // PKEY
+				"0005a5fd368978f4" + // private scalar D
+				"0000395d1827c000" + // public key Ed25519
+				"e23f657bc47ec853" + // PoW_0
+				"e23f657bc47ec9d8" +
+				"e23f657bc47ecaec" +
+				"e23f657bc47ecb29" +
+				"e23f657bc47ecc00" +
+				"e23f657bc47ecc79" +
+				"e23f657bc47ece83" +
+				"e23f657bc47ecfc6" +
+				"e23f657bc47ecfc8" +
+				"e23f657bc47ecfd5" +
+				"e23f657bc47ed02b" +
+				"e23f657bc47ed03b" +
+				"e23f657bc47ed0ff" +
+				"e23f657bc47ed241" +
+				"e23f657bc47ed264" +
+				"e23f657bc47ed2e5" +
+				"e23f657bc47ed343" +
+				"e23f657bc47ed348" +
+				"e23f657bc47ed45e" +
+				"e23f657bc47ed480" +
+				"e23f657bc47ed49a" +
+				"e23f657bc47ed564" +
+				"e23f657bc47ed565" +
+				"e23f657bc47ed5b6" +
+				"e23f657bc47ed5de" +
+				"e23f657bc47ed5e0" +
+				"e23f657bc47ed77f" +
+				"e23f657bc47ed800" +
+				"e23f657bc47ed80c" +
+				"e23f657bc47ed817" +
+				"e23f657bc47ed82c" +
+				"e23f657bc47ed8a6" + // PoW_31
+				"0396020c831a5405cee6c38842209191c8db799dbe81e0dcf6dbd4f91c257ae2" + // Sig.R
+				"0079e7fd1cd31cc24cd9a52831d5ec30f10e22e5a6dd906518746cfce2095610" + // Sig.S
+				"4ac1e51d9a585a9ad9fb0dfac2be100aee83f0cc79c4c5ea8f3eb8afd9092fa5", // PKEY
 		},
 	}
 )
