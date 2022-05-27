61b51712915b49d5943a87f85e70349a88f1d223
Bernd Fix <brf@hoi-polloi.org>
2022-05-27 20:59:35 +0200

    Reworked database framework.

--- src/gnunet/util/database.go
+++ src/gnunet/util/database.go
@@ -19,6 +19,7 @@
 package util
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"os"
@@ -34,7 +35,96 @@ var (
 	ErrSQLNoDatabase          = fmt.Errorf("Database not found")
 )
 
-// ConnectSQLDatabase connects to an SQL database (various types and flavors):
+//----------------------------------------------------------------------
+// Connection to a database instance. There can be multiple connections
+// on the same instance, managed by the database pool.
+//----------------------------------------------------------------------
+
+// DbConn is a database connection suitable for executing SQL commands.
+type DbConn struct {
+	conn *sql.Conn // connection to database instance
+	pool *dbPool   // reference to managng pool
+	key  string    // database identifier (connect string)
+}
+
+// Close database connection.
+func (db *DbConn) Close() (err error) {
+	if err = db.conn.Close(); err != nil {
+		return
+	}
+	err = db.pool.remove(db.key)
+	return
+}
+
+// QueryRow returns a single record for a query
+func (db *DbConn) QueryRow(query string, args ...any) *sql.Row {
+	return db.conn.QueryRowContext(db.pool.ctx, query, args...)
+}
+
+// Query returns all matching records for a query
+func (db *DbConn) Query(query string, args ...any) (*sql.Rows, error) {
+	return db.conn.QueryContext(db.pool.ctx, query, args...)
+}
+
+// Exec a SQL statement
+func (db *DbConn) Exec(query string, args ...any) (sql.Result, error) {
+	return db.conn.ExecContext(db.pool.ctx, query, args...)
+}
+
+// TODO: add more SQL methods
+
+//----------------------------------------------------------------------
+// DbPool holds all database instances used: Connecting with the same
+// connect string returns the same instance.
+//----------------------------------------------------------------------
+
+// global instance for the database pool (singleton)
+var (
+	DbPool *dbPool
+)
+
+// DbPoolEntry holds information about a database instance.
+type DbPoolEntry struct {
+	db      *sql.DB // reference to the database engine
+	refs    int     // number of open connections (reference count)
+	connect string  // SQL connect string
+}
+
+// package initialization
+func init() {
+	// construct database pool
+	DbPool = new(dbPool)
+	DbPool.insts = NewMap[string, *DbPoolEntry]()
+	DbPool.ctx, DbPool.cancel = context.WithCancel(context.Background())
+}
+
+// dbPool keeps a mapping between connect string and database instance
+type dbPool struct {
+	ctx    context.Context            // connection context
+	cancel context.CancelFunc         // cancel function
+	insts  *Map[string, *DbPoolEntry] // map of database instances
+}
+
+// remove a database instance from the pool based on its connect string.
+func (p *dbPool) remove(key string) error {
+	return p.insts.Process(func() (err error) {
+		// get pool entry
+		pe, ok := p.insts.Get(key)
+		if !ok {
+			return nil
+		}
+		// decrement ref count
+		pe.refs--
+		if pe.refs == 0 {
+			// no more refs: close database
+			err = pe.db.Close()
+			p.insts.Delete(key)
+		}
+		return
+	})
+}
+
+// Connect to a SQL database (various types and flavors):
 // The 'spec' option defines the arguments required to connect to a database;
 // the meaning and format of the arguments depends on the specific SQL database.
 // The arguments are seperated by the '+' character; the first (and mandatory)
@@ -46,27 +136,50 @@ var (
 // * 'mysql':   A MySQL-compatible database; the second argument specifies the
 //              information required to log into the database (e.g.
 //              "[user[:passwd]@][proto[(addr)]]/dbname[?param1=value1&...]").
-func ConnectSQLDatabase(spec string) (db *sql.DB, err error) {
-	// split spec string into segments
-	specs := strings.Split(spec, ":")
-	if len(specs) < 2 {
-		return nil, ErrSQLInvalidDatabaseSpec
-	}
-	switch specs[0] {
-	case "sqlite3":
-		// check if the database file exists
-		var fi os.FileInfo
-		if fi, err = os.Stat(specs[1]); err != nil {
-			return nil, ErrSQLNoDatabase
+func (p *dbPool) Connect(spec string) (db *DbConn, err error) {
+	err = p.insts.Process(func() error {
+		// check if we have a connection to this database.
+		inst, ok := p.insts.Get(spec)
+		if !ok {
+			inst = new(DbPoolEntry)
+			inst.refs = 0
+			inst.connect = spec
+
+			// No: create new database instance.
+			// split spec string into segments
+			specs := strings.Split(spec, ":")
+			if len(specs) < 2 {
+				return ErrSQLInvalidDatabaseSpec
+			}
+			// create database object
+			switch specs[0] {
+			case "sqlite3":
+				// check if the database file exists
+				var fi os.FileInfo
+				if fi, err = os.Stat(specs[1]); err != nil {
+					return ErrSQLNoDatabase
+				}
+				if fi.IsDir() {
+					return ErrSQLNoDatabase
+				}
+				// open the database file
+				inst.db, err = sql.Open("sqlite3", specs[1])
+			case "mysql":
+				// just connect to the database
+				inst.db, err = sql.Open("mysql", specs[1])
+			default:
+				return ErrSQLInvalidDatabaseSpec
+			}
+			// save database in pool
+			p.insts.Put(spec, inst)
+			ok = true
 		}
-		if fi.IsDir() {
-			return nil, ErrSQLNoDatabase
-		}
-		// open the database file
-		return sql.Open("sqlite3", specs[1])
-	case "mysql":
-		// just connect to the database
-		return sql.Open("mysql", specs[1])
-	}
-	return nil, ErrSQLInvalidDatabaseSpec
+		// increment reference count
+		inst.refs++
+		// return a new connection to the database.
+		db = new(DbConn)
+		db.conn, err = inst.db.Conn(p.ctx)
+		return err
+	})
+	return
 }
