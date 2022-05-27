0295198c8f0becf76e4312eb67a489114345d477
Bernd Fix <brf@hoi-polloi.org>
2022-05-27 21:02:26 +0200

    List of GANA-registered block types added.

---
+++ src/gnunet/service/dht/blocks/types.go
@@ -0,0 +1,26 @@
+// This file is part of gnunet-go, a GNUnet-implementation in Golang.
+// Copyright (C) 2019-2022 Bernd Fix  >Y<
+//
+// gnunet-go is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// gnunet-go is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: AGPL3.0-or-later
+
+package blocks
+
+// DHT Block types
+const (
+	DHT_BLOCK_ANY   = 0
+	DHT_BLOCK_HELLO = 7  // Type of a block that contains a HELLO for a peer
+	DHT_BLOCK_GNS   = 11 // Block for storing record data
+)
