d87f408ffc9b68919f33fba474b88008ef602893
Bernd Fix <brf@hoi-polloi.org>
2022-10-24 19:00:02 +0200

    results: added ResultCode type.

---
+++ src/gnunet/enums/results.go
@@ -0,0 +1,31 @@
+// This file is part of gnunet-go, a GNUnet-implementation in Golang.
+// Copyright (C) 2019-2022 Bernd Fix  >Y<
+//
+// gnunet-go is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// gnunet-go is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: AGPL3.0-or-later
+
+//nolint:stylecheck // allow non-camel-case for constants
+package enums
+
+// ResultCode type
+type ResultCode int32
+
+// ResultCode values
+const (
+	RC_SYSERR ResultCode = -1
+	RC_NO     ResultCode = 0
+	RC_YES    ResultCode = 1
+	RC_OK     ResultCode = 1
+)
