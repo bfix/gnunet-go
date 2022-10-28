8751b6af146a78b2233adb54f50922a69d5c305d
Bernd Fix <brf@hoi-polloi.org>
2022-10-28 18:08:05 +0200

    build: simplified trimpath.

--- src/gnunet/build.sh
+++ src/gnunet/build.sh
@@ -5,6 +5,4 @@ if [ "$1" = "withgen" ]; then
     shift
 fi
 
-TAG=$(git tag --sort -taggerdate | head -n 1)
-
-go install $* -trimpath -gcflags "-N -l -trimpath $(pwd)=>gnunet@${TAG}" ./...
+go install $* -trimpath -gcflags "-N -l" ./...
