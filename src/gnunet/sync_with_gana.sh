ffa5d583c941be9d07b305fa7bf7410c6cdb1c10
Bernd Fix <brf@hoi-polloi.org>
2022-10-29 12:20:38 +0200

    Synchronize registry files with GANA.

---
+++ src/gnunet/sync_with_gana.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+changed=0
+
+function update() {
+    rc=$(diff "$1" "$2")
+    if [ -n "$rc" ]; then
+        cp "$2" "$1"
+        echo "Updated registry file '$1' from '$2'"
+        changed=1
+    else
+        echo "Up-to-date registry file '$1'"
+    fi
+}
+
+# Synchronize GANA definitions
+
+REPO=../../../gana
+
+pushd $REPO
+git pull
+popd
+
+update enums/gnunet-signature.rec $REPO/gnunet-signatures/registry.rec
+update enums/gnunet-dht.rec $REPO/gnunet-dht-block-types/registry.rec
+update enums/gnunet-gns.rec $REPO/gnu-name-system-record-types/registry.rec
+
+if [ $changed -eq 1 ]; then
+    go generate ./...
+fi
